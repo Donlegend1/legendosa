2052fd9c2f26b02f9312bcdfd3ac645998950384
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-05-11 23:53:55 +0100

    updated

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,185 +17,172 @@ const Header = () => {
   return (
     <>
       <div>
-        {/* Preloader */}
-        <div className="preloader">
-          <div className="loader">
-            <div className="loader-outter" />
-            <div className="loader-inner" />
-            <div className="indicator">
-              <svg
-                width="16px"
-                height="12px">
-                <polyline
-                  id="back"
-                  points="1 6 4 6 6 11 10 1 12 6 15 6"
-                />
-                <polyline
-                  id="front"
-                  points="1 6 4 6 6 11 10 1 12 6 15 6"
-                />
-              </svg>
+        <div className="container-fluid topbar px-0 d-none d-lg-block">
+          <div className="container px-0">
+            <div
+              className="row gx-0 align-items-center"
+              style={{ height: "45px" }}>
+              <div className="col-lg-8 text-center text-lg-start mb-lg-0">
+                <div className="d-flex flex-wrap">
+                  <a
+                    href="#"
+                    className="text-muted me-4">
+                    <i className="fas fa-map-marker-alt text-primary me-2" />
+                    Find A Location
+                  </a>
+                  <a
+                    href="#"
+                    className="text-muted me-4">
+                    <i className="fas fa-phone-alt text-primary me-2" />
+                    +01234567890
+                  </a>
+                  <a
+                    href="#"
+                    className="text-muted me-0">
+                    <i className="fas fa-envelope text-primary me-2" />
+                    Example@gmail.com
+                  </a>
+                </div>
+              </div>
+              <div className="col-lg-4 text-center text-lg-end">
+                <div className="d-flex align-items-center justify-content-end">
+                  <a
+                    href="#"
+                    className="btn btn-primary btn-square rounded-circle nav-fill me-3">
+                    <i className="fab fa-facebook-f text-white" />
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-primary btn-square rounded-circle nav-fill me-3">
+                    <i className="fab fa-twitter text-white" />
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-primary btn-square rounded-circle nav-fill me-3">
+                    <i className="fab fa-instagram text-white" />
+                  </a>
+                  <a
+                    href="#"
+                    className="btn btn-primary btn-square rounded-circle nav-fill me-0">
+                    <i className="fab fa-linkedin-in text-white" />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        {/* End Preloader */}
-        {/* Get Pro Button */}
-        <ul className="pro-features">
-          <a
-            className="get-pro"
-            href="#">
-            Get Pro
-          </a>
-          <li className="big-title">Pro Version Available on Themeforest</li>
-          <li className="title">Pro Version Features</li>
-          <li>2+ premade home pages</li>
-          <li>20+ html pages</li>
-          <li>Color Plate With 12+ Colors</li>
-          <li>Sticky Header / Sticky Filters</li>
-          <li>Working Contact Form With Google Map</li>
-          <div className="button">
-            <a
-              href="http://preview.themeforest.net/item/mediplus-medical-and-doctor-html-template/full_screen_preview/26665910?_ga=2.145092285.888558928.1591971968-344530658.1588061879"
-              target="_blank"
-              className="btn">
-              Pro Version Demo
-            </a>
-            <a
-              href="https://themeforest.net/item/mediplus-medical-and-doctor-html-template/26665910"
-              target="_blank"
-              className="btn">
-              Buy Pro Version
-            </a>
-          </div>
-        </ul>
-        {/* Header Area */}
-        <header className="header">
-          {/* Topbar */}
-          <div className="topbar">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-6 col-md-5 col-12">
-                  {/* Contact */}
-                  <ul className="top-link">
-                    <li>
-                      <a href="#">About</a>
-                    </li>
-                    <li>
-                      <a href="#">Doctors</a>
-                    </li>
-                    <li>
-                      <a href="#">Contact</a>
-                    </li>
-                    <li>
-                      <a href="#">FAQ</a>
-                    </li>
-                  </ul>
-                  {/* End Contact */}
-                </div>
-                <div className="col-lg-6 col-md-7 col-12">
-                  {/* Top Contact */}
-                  <ul className="top-contact">
-                    <li>
-                      <i className="fa fa-phone" />
-                      +880 1234 56789
-                    </li>
-                    <li>
-                      <i className="fa fa-envelope" />
-                      <a href="mailto:support@yourmail.com">
-                        support@yourmail.com
-                      </a>
-                    </li>
-                  </ul>
-                  {/* End Top Contact */}
-                </div>
-              </div>
-            </div>
-          </div>
-          {/* End Topbar */}
-          {/* Header Inner */}
-          <div className="header-inner">
-            <div className="container">
-              <div className="inner">
-                <div className="row">
-                  <div className="col-lg-3 col-md-3 col-12">
-                    {/* Start Logo */}
-                    <div className="logo">
-                      <a href="index.html">
-                        <img
-                          src="img/logo.png"
-                          alt="#"
-                        />
-                      </a>
-                    </div>
-                    {/* End Logo */}
-                    {/* Mobile Nav */}
-                    <div className="mobile-nav" />
-                    {/* End Mobile Nav */}
-                  </div>
-                  <div className="col-lg-7 col-md-9 col-12">
-                    {/* Main Menu */}
-                    <div className="main-menu">
-                      <nav className="navigation">
-                        <ul className="nav menu">
-                          <li className="active">
-                            <a href="#">
-                              Home <i className="icofont-rounded-down" />
-                            </a>
-                            <ul className="dropdown">
-                              <li>
-                                <a href="index.html">Home Page 1</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="#">Doctos </a>
-                          </li>
-                          <li>
-                            <a href="#">Services </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              Pages <i className="icofont-rounded-down" />
-                            </a>
-                            <ul className="dropdown">
-                              <li>
-                                <a href="404.html">404 Error</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="#">
-                              Blogs <i className="icofont-rounded-down" />
-                            </a>
-                            <ul className="dropdown">
-                              <li>
-                                <a href="blog-single.html">Blog Details</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="contact.html">Contact Us</a>
-                          </li>
-                        </ul>
-                      </nav>
-                    </div>
-                    {/*/ End Main Menu */}
-                  </div>
-                  <div className="col-lg-2 col-12">
-                    <div className="get-quote">
+        {/* Topbar End */}
+        {/* Navbar & Hero Start */}
+        <div className="container-fluid sticky-top px-0">
+          <div
+            className="position-absolute bg-dark"
+            style={{
+              left: "0",
+              top: "0",
+              width: "100%",
+              height: "100%",
+            }}></div>
+          <div className="container px-0">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-white py-3 px-4">
+              <a
+                href="index.html"
+                className="navbar-brand p-0">
+                <h1 className="text-primary m-0">
+                  <i className="fas fa-donate me-3" />
+                  Investa
+                </h1>
+                {/* <img src="img/logo.png" alt="Logo"> */}
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarCollapse">
+                <span className="fa fa-bars" />
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarCollapse">
+                <div className="navbar-nav ms-auto py-0">
+                  <a
+                    href="index.html"
+                    className="nav-item nav-link active">
+                    Home
+                  </a>
+                  <a
+                    href="about.html"
+                    className="nav-item nav-link">
+                    About
+                  </a>
+                  <a
+                    href="service.html"
+                    className="nav-item nav-link">
+                    Services
+                  </a>
+                  <a
+                    href="project.html"
+                    className="nav-item nav-link">
+                    Projects
+                  </a>
+                  <div className="nav-item dropdown">
+                    <a
+                      href="#"
+                      className="nav-link dropdown-toggle"
+                      data-bs-toggle="dropdown">
+                      Pages
+                    </a>
+                    <div className="dropdown-menu m-0">
                       <a
-                        href="appointment.html"
-                        className="btn">
-                        Book Appointment
+                        href="blog.html"
+                        className="dropdown-item">
+                        Our Blog
+                      </a>
+                      <a
+                        href="team.html"
+                        className="dropdown-item">
+                        Our Team
+                      </a>
+                      <a
+                        href="testimonial.html"
+                        className="dropdown-item">
+                        Testimonial
+                      </a>
+                      <a
+                        href="faqs.html"
+                        className="dropdown-item">
+                        FAQs
+                      </a>
+                      <a
+                        href="404.html"
+                        className="dropdown-item">
+                        404 Page
                       </a>
                     </div>
                   </div>
+                  <a
+                    href="contact.html"
+                    className="nav-item nav-link">
+                    Contact
+                  </a>
+                </div>
+                <div className="d-flex align-items-center flex-nowrap pt-xl-0">
+                  <button
+                    className="btn btn-primary btn-md-square mx-2"
+                    data-bs-toggle="modal"
+                    data-bs-target="#searchModal">
+                    <i className="fas fa-search" />
+                  </button>
+                  <a
+                    href="#"
+                    className="btn btn-primary rounded-pill text-white py-2 px-4 ms-2 flex-wrap flex-sm-shrink-0">
+                    Start Investa
+                  </a>
                 </div>
               </div>
-            </div>
+            </nav>
           </div>
-          {/*/ End Header Inner */}
-        </header>
+        </div>
+        {/* Navbar & Hero End */}
       </div>
     </>
   );
